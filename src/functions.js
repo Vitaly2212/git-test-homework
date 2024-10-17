77224af1bf9a3793ff893e1da188739f207be5ba
Vitaly Ivanov <vetaly2212@gmail.com>
2024-10-17 17:19:11 +0300

    add

--- src/functions.js
+++
@@ -1,9 +0,0 @@
-export const greet = (name) => {
-  return `Hello, ${name}`
-}
-
-export function farewell(name) {
-  return `Goodbye, ${name}`
-}
-
-export const auth = (name, pass) => name === 'admin' && pass === 'pass'
